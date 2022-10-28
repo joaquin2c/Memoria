756dde4b2c92ad31a7b5d3f0d3706f5612c6dd44
Lukas Pavez Bahamondes <lpavezr@killa.diinf.usach.cl>
2022-10-27 22:32:59 -0300

    mod files to txt

--- files_to_txt.py
+++ files_to_txt.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 import argparse
 from os import listdir
 from os.path import isfile, join
@@ -9,25 +10,25 @@ def files_to_txt(mypath,seed):
                         'sheep', 'train', 'tvmonitor', 'bird',
                         'bus', 'cow', 'motorbike', 'sofa')
   for i in Clases_draw:
-    final_path=f"{mypath}/{i}"
+    final_path=mypath+"/"+i
     fileslist = [f[:-4] for f in listdir(final_path)]
     files_trainval,files_test=train_test_split(fileslist,test_size=0.3, random_state=seed)
     files_trainval.sort()
     files_test.sort()
-    txt_trainval_file= open(f"{final_path}/trainval.txt","w+")
+    txt_trainval_file= open(final_path+"/trainval.txt","w+")
     for files in files_trainval:
-      txt_trainval_file.write(f"{files}\n") 
+      txt_trainval_file.write(files+"\n") 
     txt_trainval_file.close()
-    txt_test_file= open(f"{final_path}/test.txt","w+")
+    txt_test_file= open(final_path+"/test.txt","w+")
     for files in files_test:
-      txt_test_file.write(f"{files}\n") 
+      txt_test_file.write(files+"\n") 
     txt_test_file.close()
 
 if __name__=="__main__":
   parser=argparse.ArgumentParser()
-  parser.add_argument('--path',help="Dirección de la carpeta draw")
-  parser.add_argument('--seed',help="Semilla de random")
-  args = parser.parse_args()
+  parser.add_argument("--path",help="Dirección de la carpeta draw")
+  parser.add_argument("--seed",help="Semilla de random")
+  args,unknown =parser.parse_known_args()
   mypath=str(args.path)
   myseed=int(args.seed)
   files_to_txt(mypath,myseed)
