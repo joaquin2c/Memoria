323e10671b3e7af998d8ff5ebca3267eb96906bb
joaquin2c <j.curimil.c@gmail.com>
2023-02-07 22:25:14 -0300

    byol

--- mmfew/mmfewshot/detection/models/backbones/__init__.py
+++ mmfew/mmfewshot/detection/models/backbones/__init__.py
@@ -1,5 +1,6 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .resnet_with_meta_conv import ResNetWithMetaConv
-from .BYOL1 import BYOLDRAW
+from .byol_model import BYOL
+from .byol2_model import BYOL2
 
-__all__ = ['ResNetWithMetaConv','BYOLDRAW']
+__all__ = ['ResNetWithMetaConv','BYOL','BYOL2']
