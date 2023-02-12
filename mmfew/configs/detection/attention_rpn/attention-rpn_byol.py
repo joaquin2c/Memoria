c5da2aea9fe1f4e1104d8a04b88b1bcb7b8166cf
joaquin2c <j.curimil.c@gmail.com>
2023-02-12 02:38:01 -0300

    byol

--- mmfew/configs/detection/attention_rpn/attention-rpn_byol.py
+++ mmfew/configs/detection/attention_rpn/attention-rpn_byol.py
@@ -6,23 +6,23 @@ num_support_shots = 1
 # model settings
 model = dict(
     type='AttentionRPNDetector',
-    rpn_head=dict(
+        rpn_head=dict(
         type='AttentionRPNHead',
-        in_channels=2048,
-        feat_channels=2048,
+        in_channels=1024,
+        feat_channels=1024,
         num_support_ways=num_support_ways,
         num_support_shots=num_support_shots,
         roi_extractor=dict(
             type='SingleRoIExtractor',
             roi_layer=dict(type='RoIAlign', output_size=14, sampling_ratio=0),
-            out_channels=256,
+            out_channels=1024,
             featmap_strides=[16]),
         aggregation_layer=dict(
             type='AggregationLayer',
             aggregator_cfgs=[
                 dict(
                     type='DepthWiseCorrelationAggregator',
-                    in_channels=256,
+                    in_channels=1024,
                     with_fc=False)
             ])),
     roi_head=dict(
@@ -32,7 +32,7 @@ model = dict(
         bbox_roi_extractor=dict(
             type='SingleRoIExtractor',
             roi_layer=dict(type='RoIAlign', output_size=14, sampling_ratio=0),
-            out_channels=256,
+            out_channels=1024,
             featmap_strides=[16]),
         bbox_head=dict(
             type='MultiRelationBBoxHead',
